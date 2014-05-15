616ecdfd2d781a2dad5005ef2ad7a6a0eeac96fa
Ben Hudson <bahhudson@gmail.com>
2014-05-14 23:08:16 -0400

    Update posts controller to always return json.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,23 @@ class PostsController < ApplicationController
 	respond_to :json
 	
 	def index
-		respond_with Post.all
+		render json: Post.all
 	end
 
 	def show
-		respond_with Post.find(params[:id])
+		render json: Post.find(params[:id])
 	end
 
 	def create
-		respond_with Post.create(post_params)
+		render json: Post.create(post_params)
 	end
 
 	def update
-		respond_with Post.update(params[:id], post_params)
+		render json: Post.update(params[:id], post_params)
 	end
 
 	def destroy
-		respond_with Post.destroy(params[:id])
+		render json: Post.destroy(params[:id])
 	end
 
 	private def post_params
